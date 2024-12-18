8b36c3a5ae2d489bee4ccbb1fca591f3fbc20e24
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2024-12-18 11:11:18 +0530

    Add files via upload

---
+++ Difficulty rating wise/500 difficulty rating/Audible Range.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int tt, num;
+    cin >> tt;
+    for(int i = 0; i < tt; i++){
+        cin >> num;
+        if ( num >= 67 && num <= 45000 ){
+            cout << "YES" << '\n';
+        }else{
+            cout << "NO" << '\n';
+        }
+    }
+	// your code goes here
+
+}
---
+++ Difficulty rating wise/500 difficulty rating/IPL Ticket Rush.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int tt, x, y;
+    cin >> tt;
+    while(tt--){
+        cin >> x >> y;
+        if(x - y > 0){
+            cout << x - y << '\n';
+        }
+        else{
+            cout << 0 << '\n';
+        }
+    }
+	// your code goes here
+
+}
