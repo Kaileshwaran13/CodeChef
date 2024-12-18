962f9e45d9813b4f76f46a3839dbb8fe2ecfeb34
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2024-12-18 11:04:00 +0530

    Add files via upload

---
+++ difficultyRatingWise/500_Difficulty_Rating/Masterchef finals.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int tt, x;
+    cin >> tt;
+    for(int i = 0; i < tt; i++){
+        cin >> x;
+        if(x <= 10){
+            cout << "YES" << '\n';
+        }
+        else{
+            cout << "NO" << '\n';
+        }
+    }
+	// your code goes here
+
+}
---
+++ difficultyRatingWise/500_Difficulty_Rating/Saving Taxes.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int tt, x, y;
+    cin >> tt;
+    for(int i = 0; i < tt; i++){
+        cin >> x >> y;
+        cout << x - y << '\n';
+    }
+    
+	// your code goes here
+
+}
