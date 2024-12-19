f4d0f0d6f66b0a64ccae7c3868b7d9c048984914
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2024-12-19 06:44:15 +0530

    Add files via upload

---
+++ Difficulty rating wise/500 difficulty rating/Candy Division.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int tt, num;
+    cin >> tt;
+    while(tt--){
+        cin >> num;
+        if(num % 3 == 0){
+            cout << "YES\n";
+        }
+        else{
+            cout << "NO\n";
+        }
+    }
+
+}
+
