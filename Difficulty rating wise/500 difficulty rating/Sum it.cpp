d938b5ca4ea04af56768c58b77aff028b783a8d8
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2024-12-21 20:29:37 +0530

    Create Sum it.cpp

---
+++ Difficulty rating wise/500 difficulty rating/Sum it.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int tt, a, b, c;
+    cin >> tt;
+    while(tt--){
+        cin >> a >> b >> c;
+        if(a + b == c){
+            cout << "YES" << '\n';
+        }
+        else{
+            cout << "NO" << '\n';
+        }
+    }
+	// your code goes here
+
+}
