41f9ea8b38d65114299fa8172130e24d1b9932ac
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2024-12-21 22:39:24 +0530

    Create Volume Control.cpp

---
+++ Difficulty rating wise/500 difficulty rating/Volume Control.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int tt, x, y;
+    cin >> tt;
+    while(tt--){
+        cin >> x >> y;
+        cout << max(x,y) - min(x,y) << '\n';
+    }
+}
