491073af97de4869c107441539b18e5cea30b234
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2024-12-21 20:17:40 +0530

    Create Total Prize Money.cpp

---
+++ Difficulty rating wise/500 difficulty rating/Total Prize Money.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int tt, x, y;
+    cin >> tt;
+    while(tt--){
+        cin >> x >> y;
+        cout << (x * 10) + (y * 90) << '\n';
+    }
+	// your code goes here
+
+}
