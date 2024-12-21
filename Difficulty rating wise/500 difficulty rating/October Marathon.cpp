54e88f9a5d1caa307da610881c0bea21d7b56414
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2024-12-21 20:31:49 +0530

    Create October Marathon.cpp

---
+++ Difficulty rating wise/500 difficulty rating/October Marathon.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int num;
+    cin >> num;
+    if(num < 3){
+        cout << "GOLD" << '\n';
+    }
+    else if(num < 6){
+        cout << "SILVER" << '\n';
+    }
+    else{
+        cout << "BRONZE" << '\n';
+    }
+	// your code goes here
+
+}
