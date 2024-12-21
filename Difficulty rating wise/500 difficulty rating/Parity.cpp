0ccad59dc66750a95ee62f38d5d4ab1b6cbf4275
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2024-12-21 20:16:54 +0530

    Create Parity.cpp

---
+++ Difficulty rating wise/500 difficulty rating/Parity.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int tt, num;
+    cin >> tt;
+    while(tt--){
+        cin >> num;
+        if(num % 2 == 0){
+            cout << "Yes" << '\n';
+        }
+        else{
+            cout << "No" << '\n';
+        }
+    }
+	// your code goes here
+
+}
