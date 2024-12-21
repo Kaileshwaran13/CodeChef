5f692c8cf6fb75dca04af57c692b8d5dcba0b843
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2024-12-21 20:29:59 +0530

    Create Get Subscription.cpp

---
+++ Difficulty rating wise/500 difficulty rating/Get Subscription.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int tt, x;
+    cin >> tt;
+    while(tt--){
+        cin >> x;
+        if(x <= 30){
+            cout << "NO" << '\n';
+        }
+        else{
+            cout << "YES" << '\n';
+        }
+    }
+	// your code goes here
+
+}
