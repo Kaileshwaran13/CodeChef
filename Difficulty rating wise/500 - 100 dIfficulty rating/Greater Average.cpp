885c74b03c054466c53f55bb51ef301b71b71175
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2024-12-22 08:36:46 +0530

    Add files via upload

---
+++ Difficulty rating wise/500 - 100 dIfficulty rating/Greater Average.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int tt, x, y, z;
+    float avg;
+    cin >> tt;
+    while(tt--){
+        cin >> x >> y >> z;
+        avg = (x + y) / 2.0;
+        if(avg > z){
+            cout << "YES" << '\n';
+        }else{
+            cout << "NO" << '\n';
+        }
+    }
+}
+
+
