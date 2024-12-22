6882be3766aecd5d814d9b517582e8df9bd29277
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2024-12-22 11:06:56 +0530

    Create Increase IQ.cpp

---
+++ Difficulty rating wise/500 difficulty rating/Increase IQ.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int num;
+    cin >> num;
+    if(num + 7 > 170){
+        cout << "Yes" << '\n';
+    }
+    else{
+        cout << "No" << '\n';
+    }
+
+}
