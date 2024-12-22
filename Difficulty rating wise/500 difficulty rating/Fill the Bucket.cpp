b19d3edc1a2fef3858e62ce0894eb0719b7f7a9f
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2024-12-22 08:43:48 +0530

    Create Fill the Bucket.cpp

---
+++ Difficulty rating wise/500 difficulty rating/Fill the Bucket.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int tt, x, y;
+    cin >> tt;
+    while(tt--){
+        cin >> x >> y;
+        cout << x - y << '\n';
+    }
+}
