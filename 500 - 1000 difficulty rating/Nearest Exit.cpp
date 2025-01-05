5916d2bf11e1592f7d6fb8766c130127133cdc03
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2025-01-05 19:34:33 +0530

    Create Nearest Exit.cpp

---
+++ 500 - 1000 difficulty rating/Nearest Exit.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int tt, num;
+    cin >> tt;
+    while(tt--){
+        cin >> num;
+        if(num <= 50){
+            cout << "LEFT" <<'\n';
+        }
+        else{
+            cout << "RIGHT" << '\n';
+        }
+    }
+
+}
