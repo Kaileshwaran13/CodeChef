056813da32035a805f65a57258e103c408ef33fc
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2025-01-06 18:59:56 +0530

    Create Finding Square Roots.cpp

---
+++ 500 - 1000 difficulty rating/Finding Square Roots.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int tt, num;
+    cin >> tt;
+    while(tt--){
+        cin >>num;
+        cout << floor(sqrt(num)) <<'\n';
+    }
+
+}
