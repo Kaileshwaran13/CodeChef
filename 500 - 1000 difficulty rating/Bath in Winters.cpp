4d5fa396cfc1dec5503f208028ca37c597adc6af
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2025-01-06 13:58:00 +0530

    Create Bath in Winters.cpp

---
+++ 500 - 1000 difficulty rating/Bath in Winters.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int tt, x, y;
+    cin >> tt;
+    while(tt--){
+        cin >> x >> y;
+        cout << x / (y * 2) <<'\n';
+    }
+
+}
