92518651976066981c21f13413a0efd74da515e2
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2025-01-06 14:25:14 +0530

    Create Small factorials.cpp

---
+++ 500 - 1000 difficulty rating/Small factorials.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include<boost/multiprecision/cpp_int.hpp>
+using namespace std;
+using namespace boost::multiprecision;
+
+int main() {
+    int tt, num;
+    cpp_int product;
+    cin >> tt;
+    while(tt--){
+        cin >> num;
+        product = 1;
+        while(num > 0 ){
+            product *= num;
+            num--;
+        }
+        cout << product << '\n';
+    }
+
+}
