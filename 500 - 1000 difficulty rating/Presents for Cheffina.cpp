ac1f90832321bf8a04aac3ac0a5a28faf21107d9
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2025-01-07 14:03:59 +0530

    Create Presents for Cheffina.cpp

---
+++ 500 - 1000 difficulty rating/Presents for Cheffina.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int tt, num;
+    cin >> tt;
+    while(tt--){
+        cin >> num;
+        cout << num - (num / 5) << '\n';
+    }
+
+}
