b65abafd618014c96287099c681614485befaa0c
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2025-01-08 19:56:27 +0530

    Create Genes.cpp

---
+++ 500 - 1000 difficulty rating/Genes.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    char ch1, ch2;
+    cin >>ch1 >> ch2;
+    if( ch1 == 'R' || ch2 == 'R'){
+        cout << "R";
+    }
+    else if( ch1 == 'B' || ch2 == 'B'){
+        cout << "B";
+    }
+    else{
+        cout << "G";
+    }
+
+}
