7c6148676bb6e6a4d5b1a5e019b49796da450491
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2025-01-08 18:47:11 +0530

    Create Summer Heat.cpp

---
+++ 500 - 1000 difficulty rating/Summer Heat.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int tt, a, b, x, y;
+    cin >> tt;
+    while(tt--){
+        cin >> a >> b >> x >> y;
+        cout << (x / a) + (y / b) <<'\n';
+    }
+
+}
