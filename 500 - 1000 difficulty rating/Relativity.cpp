5f0db06ce82c9092bfedcfb3a6c87b33e2b11812
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2025-01-08 20:31:29 +0530

    Create Relativity.cpp

---
+++ 500 - 1000 difficulty rating/Relativity.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int tt, c, g;
+    cin >> tt;
+    while(tt--){
+        cin >> g >> c;
+        cout << (c * c) / (2 * g) <<'\n';
+    }
+
+}
