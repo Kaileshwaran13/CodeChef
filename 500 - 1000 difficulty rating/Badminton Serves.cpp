40295a8457505334ea40bf4902f5edf5e25155ec
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2025-01-08 20:20:12 +0530

    Create Badminton Serves.cpp

---
+++ 500 - 1000 difficulty rating/Badminton Serves.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int tt, num;
+    cin >>tt;
+    while(tt--){
+        cin >> num;
+        cout << (num / 2) + 1 <<'\n';
+    }
+
+}
