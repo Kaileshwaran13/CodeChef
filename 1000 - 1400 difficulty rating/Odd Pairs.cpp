c25ba6db457aa158aa8348cee6624574e915cf4d
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2025-01-10 20:46:27 +0530

    Create Odd Pairs.cpp

---
+++ 1000 - 1400 difficulty rating/Odd Pairs.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int tt;
+    long long num;
+    cin >> tt;
+    while(tt--){
+        cin >> num;
+        cout << num * num / 2 << '\n';
+    }
+
+}
