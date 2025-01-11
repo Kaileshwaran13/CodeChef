5c3585128c7e111d8f3787f35a500fc00e992985
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2025-01-11 13:17:41 +0530

    Create Covid and Theatre Tickets.cpp

---
+++ 1000 - 1400 difficulty rating/Covid and Theatre Tickets.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int tt, n, m;
+    cin >> tt;
+    while(tt--){
+        cin >> n >> m;
+        cout << ((n + 1) / 2) * ((m + 1) / 2) << '\n';
+    }
+
+}
