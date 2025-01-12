eaff6b3e60c8aeecab1e32013e66e3fa3187b25c
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2025-01-12 23:20:10 +0530

    Create Average of Three.cpp

---
+++ 1000 - 1400 difficulty rating/Average of Three.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int tt, x;
+    cin >> tt;
+    while(tt--){
+        cin >> x;
+        cout << x - 1 << " " << x << x + 1 << '\n';
+    }
+
+}
