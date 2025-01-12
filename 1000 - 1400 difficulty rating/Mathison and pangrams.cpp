ae0dc8a8e29e360662978e34d2a1d45beaf8c56d
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2025-01-12 23:46:10 +0530

    Create Mathison and pangrams.cpp

---
+++ 1000 - 1400 difficulty rating/Mathison and pangrams.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int tt;
+    cin >> tt;
+    while (tt--) {
+        int price[26];
+        for (int i = 0; i < 26; i++) {
+            cin >> price[i];
+        }
+        string s;
+        cin >> s;
+        int c = 0;
+        for (int i = 'a'; i <= 'z'; i++) {
+            bool f = false;
+            for (int j = 0; j < s.length(); j++) {
+                if (i == s[j]) {
+                    f = true;
+                }
+            }
+            if (f == false) {
+                c += price[i - 97];
+            }
+        }
+        cout << c << endl;
+    }
+}
