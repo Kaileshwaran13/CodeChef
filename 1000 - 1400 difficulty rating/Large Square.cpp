57f9dc0abc6f588d88120a942e7515970c5c90e9
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2025-01-12 23:42:02 +0530

    Create Large Square.cpp

---
+++ 1000 - 1400 difficulty rating/Large Square.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int tt, a, b;
+    cin >> tt;
+    while(tt--){
+        cin >> a >> b;
+        int prod = sqrt(a);
+        cout << prod * b << '\n';
+    }
+
+}
