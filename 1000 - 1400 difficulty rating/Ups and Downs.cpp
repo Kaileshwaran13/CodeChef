1a21278eff96741941ea6c41bbb916e34df514df
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2025-01-13 21:23:02 +0530

    Create Ups and Downs.cpp

---
+++ 1000 - 1400 difficulty rating/Ups and Downs.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    int a[n];
+	    for(int i=0; i<n; i++)
+	    {
+	        cin>>a[i];
+	    }
+	    sort(a, a+n);
+	    int r;
+	    for(int i=1; i<n-1 ;i++)
+	    {
+	        r = a[i];
+	        a[i] = a[i+1];
+	        a[i+1] = r;
+	        i++;
+	    }
+	    for(int i=0; i<n; i++)
+	    {
+	        cout<<a[i]<<" ";
+	    }
+	    cout<<'\n';
+	}
+}
