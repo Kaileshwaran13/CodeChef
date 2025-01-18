ecf7f7096f3722d4722deda711c9506f607e96ce
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2025-01-18 23:44:32 +0530

    Create Difference Matrix.cpp

---
+++ 1000 - 1400 difficulty rating/Difference Matrix.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int cnt = 0;
+        for (int i = 1; i <= n * n; i = i + 2) {
+            cout << i << " ";
+            cnt++;
+            if (cnt == n) {
+                cout << '\n';
+                cnt = 0;
+            }
+        }
+        for (int i = 2; i <= n * n; i = i + 2) {
+            cout << i << " ";
+            cnt++;
+            if (cnt == n) {
+                cout << '\n';
+                cnt = 0;
+            }
+        }
+        cout << '\n';
+    }
+}
