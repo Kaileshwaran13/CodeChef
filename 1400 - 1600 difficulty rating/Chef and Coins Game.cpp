aba1eed79d02d2fcfc937c5743b5169ae3070e90
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2025-01-19 19:03:30 +0530

    Create Chef and Coins Game.cpp

---
+++ 1400 - 1600 difficulty rating/Chef and Coins Game.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int tt, num;
+    cin >> tt;
+    while(tt--){
+        cin >> num;
+        if( num % 6 == 0){
+            cout << "Misha" <<'\n';
+        }
+        else{
+            cout << "Chef" <<'\n';
+        }
+    }
+
+}
