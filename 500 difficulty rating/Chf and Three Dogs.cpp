d0cfec6998561250ff946898f1da0cbee85585b3
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2025-01-20 17:15:36 +0530

    Create Chf and Three Dogs.cpp

---
+++ 500 difficulty rating/Chf and Three Dogs.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int tt;
+    cin >>tt;
+    while(tt--){
+        double num1, num2;
+        cin >> num1 >> num2;
+        cout << fixed << setprecision(6) << (2 * num1)/ (3 * num2) <<'\n';
+    }
+
+}
