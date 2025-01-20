4a6cf20803f0195f16ed8fe41ece4c24a9c00efa
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2025-01-20 23:31:48 +0530

    Update Jogging.cpp

--- 1400 - 1600 difficulty rating/Jogging.cpp
+++ 1400 - 1600 difficulty rating/Jogging.cpp
@@ -4,21 +4,18 @@ using namespace std;
 #define mod 1000000007
 
 int power(int a, int b) {
-    if (b == 0) 
-        return 1;
+    if (b == 0) return 1;
     int value = power(a, b / 2);
-    if (b % 2 == 0) 
-        return (value * value) % mod;
-    else 
-        return (value * value * a) % mod;
+    if (b % 2 == 0) return (value * value) % mod;
+    else return (value * value * a) % mod;
 }
 
-int main() {
+int32_t main() {
     int t;
     cin >> t;
     while (t--) {
         int n, k;
         cin >> n >> k;
-        cout << (k * power(2, n - 1)) % mod << '\n';
+        cout << (k * power(2, n - 1)) % mod << endl;
     }
 }
