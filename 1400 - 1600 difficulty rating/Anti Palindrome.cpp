06ca2e143340b816c066acd4e8fa40748ba993f6
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2025-01-20 23:49:52 +0530

    Create Anti Palindrome.cpp

---
+++ 1400 - 1600 difficulty rating/Anti Palindrome.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, temp = 0;
+        cin >> n;
+        string s;
+        cin >> s;
+        int hash[26] = {
+            0
+        };
+        for (char c: s)
+            hash[c - 'a']++;
+        for (int i = 0; i < 26; i++) {
+            if (hash[i] > 0)
+                temp++;
+        }
+        int palindrome = 0;
+        if (s.length() % 2 == 0) {
+            bool p = true;
+            for (int i = 0; i < 26; i++) {
+                if (hash[i] % 2 != 0)
+                    p = false;
+            }
+            if (p)
+                palindrome++;
+        }
+        else {
+            int odd = 0;
+            for (int i = 0; i < 26; i++) {
+                if (hash[i] % 2 != 0)
+                    odd++;
+            }
+            if (odd == 1)
+                palindrome++;
+        }
+        if (temp == 1)
+        {
+            if (s.length() % 2 == 0)
+                cout << 1 << '\n';
+            else
+                cout << 2 << '\n';
+        }
+        else if (palindrome == 1)
+            cout << 1 << '\n';
+        else if (palindrome == 0)
+            cout << 0 << '\n';
+    }
+}
