a0b01e34189d7970dc810a63578b9bd7b25eecad
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2025-01-22 21:00:44 +0530

    Create 50-50 Rule.cpp

---
+++ 1000 - 1400 difficulty rating/50-50 Rule.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,m;
+	    cin>>a>>m;
+	    if(a<50) cout<<"Z"<<endl;
+	    else if(m<50) cout<<"F"<<endl;
+	    else cout<<"A"<<endl;
+	}
+
+}
