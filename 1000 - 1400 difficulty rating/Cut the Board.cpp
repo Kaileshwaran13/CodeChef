2c0f114e26800074ae1c173d402dc834923eb066
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2025-01-22 21:09:23 +0530

    Create Cut the Board.cpp

---
+++ 1000 - 1400 difficulty rating/Cut the Board.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int n,m;
+    cin>>n>>m;
+    
+    cout<<(n-1)*(m-1)<<endl;
+    
+}
+int main() {
+    int n;
+    cin>>n;
+    while(n--){
+        solve();
+    }
+	return 0;
+}
