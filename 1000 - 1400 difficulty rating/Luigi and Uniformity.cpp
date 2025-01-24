6f07b78b0898e8db6393d9641055bbdb3a240f47
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2025-01-24 21:41:39 +0530

    Create Luigi and Uniformity.cpp

---
+++ 1000 - 1400 difficulty rating/Luigi and Uniformity.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        sort(a, a + n);
+        int nu = a[0], ans = 0;
+        for (int i = n - 1; i > 0; i--)
+        {
+            if (a[i] > nu)
+            {
+                if(gcd(nu, a[i]) != nu)
+                {
+                    ans = n;
+                    break;
+                }
+            else
+            {
+                ans++;
+            }
+            }
+        }
+        cout << ans << endl;
+    }
+}
