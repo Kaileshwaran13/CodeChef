dc48e331427c68516229115e1730afc8d5abc298
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2025-01-25 20:37:23 +0530

    Create Valentine Gifts.cpp

---
+++ 500 - 1000 difficulty rating/Valentine Gifts.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--> 0)
+    {
+        int x;
+        cin >> x;
+        if (x < 127)
+        {
+            cout << "NO" << endl;
+        }
+        else
+        {
+            cout << "YES" << endl;
+        }
+    }
+
+}
