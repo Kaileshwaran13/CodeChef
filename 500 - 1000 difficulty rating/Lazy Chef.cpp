dd0e673fd54a221117e65539c6165d0b78b86fa2
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2025-01-26 21:51:52 +0530

    Create Lazy Chef.cpp

---
+++ 500 - 1000 difficulty rating/Lazy Chef.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int x, m, d;
+        cin >> x >> m >> d;
+        int n = x * m;
+        int b = x + d;
+        if (n <= b)
+            cout << n << endl;
+        else if (n > d)
+            cout << b << endl;
+    }
+    return 0;
+}
