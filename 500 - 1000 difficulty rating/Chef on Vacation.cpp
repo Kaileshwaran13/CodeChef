6481293c0a04461b1f35c272f5069a03aaf4d8bf
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2025-01-27 20:15:42 +0530

    Create Chef on Vacation.cpp

---
+++ 500 - 1000 difficulty rating/Chef on Vacation.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int tt, x, y, z;
+    cin >> tt;
+    while(tt--){
+        cin >> x >> y >> z;
+        if(x + y <= z)
+            cout << "YES" << '\n';
+        else
+            cout << "NO" <<'\n';
+    }
+
+}
