f184fc3fb4400d1cc9a60dd8bd1f67bb1245dc18
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2025-01-28 19:34:47 +0530

    Create Chess Pairing.cpp

---
+++ 500 - 1000 difficulty rating/Chess Pairing.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int n, x;
+        cin >> n >> x;
+        int ans = 0;
+        if (2 * n == x) {
+            ans = x;
+        }
+        else if (x > n) {
+            int p = 2 * n - x;
+            ans = -(p - x);
+        }
+        cout << ans << endl;
+    }
+
+    return 0;
+}
