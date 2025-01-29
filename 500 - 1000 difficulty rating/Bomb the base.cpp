8dadc4764e45c37bcf99bf561fe832387f7d4802
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2025-01-29 20:34:12 +0530

    Create Bomb the base.cpp

---
+++ 500 - 1000 difficulty rating/Bomb the base.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, x;
+        cin >> n >> x;
+        int num, res = 0;
+        for (int i = 1; i <= n; i++) {
+            cin >> num;
+            if (num < x) res = i;
+        }
+        cout << res << endl;
+    }
+    return 0;
+}
