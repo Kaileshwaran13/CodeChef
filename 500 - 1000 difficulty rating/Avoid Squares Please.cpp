ab98335ebb4240dab5a40c68528da03779d5c11e
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2025-01-29 20:36:57 +0530

    Create Avoid Squares Please.cpp

---
+++ 500 - 1000 difficulty rating/Avoid Squares Please.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // your code goes here
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        while (n) {
+            cout << n << " ";
+            n--;
+        }
+        cout << endl;
+    }
+    return 0;
+}
