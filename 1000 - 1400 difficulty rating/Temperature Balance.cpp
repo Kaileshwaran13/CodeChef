4d2b85617f671222d03455991d52619e49940fe8
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2025-01-31 23:32:15 +0530

    Create Temperature Balance.cpp

---
+++ 1000 - 1400 difficulty rating/Temperature Balance.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector < int > v(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> v[i];
+        }
+        long long steps = v[0];
+        long long time = 0;
+        for (int i = 1; i < n; i++)
+        {
+            time += abs(steps);
+            steps += v[i];
+        }
+        cout << time << endl;
+    }
+}
