d1a098fd5550cdc7e4c3575173f0f855ec96001e
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2025-01-31 23:31:31 +0530

    Create Dazzling AXNODR Challenge.cpp

---
+++ 1000 - 1400 difficulty rating/Dazzling AXNODR Challenge.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long t;
+    cin >> t;
+    while (t--)
+    {
+        long long n;
+        cin >> n;
+        if (n % 4 == 2 || n % 4 == 3)
+        {
+            cout << "3" << endl;
+        }
+        else if (n % 4 == 0)
+        {
+            cout << n + 3 << endl;
+        }
+        else {
+            cout << n << endl;
+        }
+    }
+    return 0;
+}
