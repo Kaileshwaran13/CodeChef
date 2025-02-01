13406bf053d8825382baeb88a0b4156706c16551
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2025-02-01 19:51:26 +0530

    Create False Number.cpp

---
+++ 1000 - 1400 difficulty rating/False Number.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string x;
+        cin >> x;
+        int len = x.length();
+        if (x[0] == '1')
+        {
+            cout << "10";
+            for (int i = 1; i < len; i++)
+            {
+                cout << x[i];
+
+            }
+            cout << endl;
+        }
+        else
+        {
+            cout << "1" << x << endl;
+        }
+    }
+    return 0;
+}
