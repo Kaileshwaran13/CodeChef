4c7ef91bdb861a1151e47bb001a702ea3288babe
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2025-02-01 17:34:05 +0530

    Create At the Gates.cpp

---
+++ 1000 - 1400 difficulty rating/At the Gates.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        int cH = 0;
+
+
+        vector < char > toss(n);
+        for (int i = 0; i < n; i++) {
+            cin >> toss[i];
+        }
+
+        for (int i = n - 1; i >= n - k; i--) {
+            if (toss[i] == 'H') {
+                //flip
+                for (int j = 0; j < i; j++) {
+                    if (toss[j] == 'H') toss[j] = 'T';
+                    else toss[j] = 'H';
+                }
+            }
+        }
+
+        for (int i = 0; i < n - k; i++) {
+            // cout<<toss[i]<<" ";
+            if (toss[i] == 'H') cH++;
+        }
+
+        cout << cH << endl;
+
+    }
+
+}
