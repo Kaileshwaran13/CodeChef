4a8de86a7134ff944916bfa05055d6bbd6d5357a
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2025-02-01 17:30:24 +0530

    Create Check Algorithmn.cpp

---
+++ 1000 - 1400 difficulty rating/Check Algorithmn.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+signed main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s, p;
+        cin >> s;
+        int x = s.size();
+        s.push_back('A');
+        int count = 1;
+        for (int i = 1; i <= x; i++) {
+            if (s[i - 1] == s[i]) {
+                count++;
+            }
+            else {
+                p += s[i - 1];
+                p += to_string(count);
+                count = 1;
+            }
+        }
+        int y = p.size();
+        if (x > y) {
+            cout << "YES" << endl;
+        }
+        else {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
