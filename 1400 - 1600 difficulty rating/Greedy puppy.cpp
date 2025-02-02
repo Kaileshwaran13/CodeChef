a3f1b755f342d33b586833ba47915e0879cac5c0
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2025-02-02 21:35:58 +0530

    Create Greedy puppy.cpp

---
+++ 1400 - 1600 difficulty rating/Greedy puppy.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n = 0, k = 0;
+        cin >> n >> k;
+        int max = 0;
+        int h = k;
+        while (k > h / 2)
+        {
+            if (n % k > max) 
+                max = n % k;
+            k--;
+        }
+
+        cout << max << endl;
+
+    }
+    return 0;
+}
