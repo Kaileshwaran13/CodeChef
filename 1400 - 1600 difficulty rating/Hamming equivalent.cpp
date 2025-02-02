b073245587dd831ad9ff62c9e56e03e5ef1ab8ef
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2025-02-02 22:39:13 +0530

    Create Hamming equivalent.cpp

---
+++ 1400 - 1600 difficulty rating/Hamming equivalent.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        long long N;
+        cin >> N;
+        vector < long long > P(N);
+        long long b = 0;
+        for (int i = 0; i < N; ++i) {
+            cin >> P[i];
+            if (__builtin_popcountll((long long)(i + 1)) != __builtin_popcountll(P[i])) {
+                b++;
+            }
+        }
+        if (b == 1) 
+            cout << "No" << endl;
+        else 
+            cout << "Yes" << endl;
+
+
+    }
+
+    return 0;
+}
