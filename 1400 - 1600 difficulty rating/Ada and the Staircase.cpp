8bd4710f052893e4e9a8e36de3cdf30f1ef7811f
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2025-02-02 23:07:24 +0530

    Create Ada and the Staircase.cpp

---
+++ 1400 - 1600 difficulty rating/Ada and the Staircase.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, k;
+        cin >> n >> k;
+        int h[n + 1];
+        int count = 0;
+        h[0] = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            cin >> h[i];
+        }
+        for (int i = 1; i <= n; i++)
+        {
+            int diff = h[i] - h[i - 1];
+            if (diff > k)
+            {
+                count += (diff - 1) / k;
+            }
+        }
+        cout << count << endl;
+    }
+}
