b1f2adfcdb9e36d76fd4791c67eff773cf1c0441
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2025-02-03 19:42:22 +0530

    Create Distinct Characters Subsequence.cpp

---
+++ 1000 - 1400 difficulty rating/Distinct Characters Subsequence.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+      int t;
+      cin >> t;
+      while(t--)
+      {
+          string s;
+          cin >> s;
+          int n = s.size();
+          unordered_set<char>v;
+          for(int j=0;j<n;j++)
+          {
+              v.insert(s[j]);
+          }
+          cout << v.size() << '\n';
+      }
+	return 0;
+}
