fa99e8d42004c26f45dda9107c52b8444d8aa27e
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2025-02-03 19:58:10 +0530

    Create Cleaning Up.cpp

---
+++ 1000 - 1400 difficulty rating/Cleaning Up.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, m;
+        cin >> n >> m;
+        int arr[m];
+        for (int i = 0; i < m; i++)
+            cin >> arr[i];
+        sort(arr, arr + m);
+        vector<int> chef, assi;
+        int i = 1, idx = 0, btn = 0;
+        while (i <= n) 
+        {
+            if (!btn && (arr[idx] != i)) 
+            {
+                chef.push_back(i); 
+                btn = 1;           
+            }
+            else if (btn && (arr[idx] != i)) 
+            {
+                assi.push_back(i);
+                btn = 0;
+            }
+            else 
+                idx++;
+            i++;
+        }
+        if (!(chef.size()))
+            cout << "-1";
+        for (int i = 0; i < chef.size() + assi.size(); i++)
+        {
+            if (i < chef.size())
+                cout << chef[i] << ' ';
+            else
+            {
+                if (!(i - chef.size()))
+                    cout << endl;
+                cout << assi[i - chef.size()] << ' ';
+            }
+        }
+        if (!(assi.size()))
+            cout << "\n-1";
+        cout << endl;
+    }
+    return 0;
+}
