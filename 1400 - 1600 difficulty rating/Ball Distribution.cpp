d1595e57239c2cffbdc7586a01301842b4512cd3
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2025-02-05 19:55:29 +0530

    Create Ball Distribution.cpp

---
+++ 1400 - 1600 difficulty rating/Ball Distribution.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++) {
+        int n, m;
+        cin >> n >> m;
+        int a[m];
+        int sum = 0;
+        for (int j = 0; j < m; j++) {
+            cin >> a[j];
+            sum += a[j];
+        }
+        int ans = n * (m - 1);
+        if ((sum - ans) < 0) {
+            cout << 0 << endl;
+        }
+        else {
+            cout << sum - ans << endl;
+        }
+
+    }
+    return 0;
+}
