6e7f60253f19a3ab935e64e90b0a631bf06701dc
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2025-02-05 23:20:10 +0530

    Create Maximum Light Up.cpp

---
+++ 1400 - 1600 difficulty rating/Maximum Light Up.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int int64_t
+
+int32_t main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, a, b, c, x, y;
+        cin >> n >> a >> b >> c >> x >> y;
+        int xx = (x * a) + b;
+        int yy = (y * a) + c;
+        int div = min({xx, yy});
+        cout << n / div << endl;
+    }
+}
