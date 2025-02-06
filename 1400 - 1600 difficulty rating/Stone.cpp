48d80decf2d2395f70abc8ce8c5e8645afec1695
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2025-02-06 14:41:24 +0530

    Create Stone.cpp

---
+++ 1400 - 1600 difficulty rating/Stone.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+int main() {
+    ll n, k;
+    cin >> n >> k;
+    ll arr[n];
+    for (int i = 0; i < n; i++) cin >> arr[i];
+    if (k == 0)
+        k = 0;
+    else if (k % 2 == 0)
+        k = 2;
+    else
+        k = 1;
+    while (k--) {
+        ll mx = * max_element(arr, arr + n);
+        for (int i = 0; i < n; i++) {
+            arr[i] = (mx - arr[i]);
+        }
+    }
+    for (int i = 0; i < n; i++) 
+        cout << arr[i] << " ";
+    return 0;
+}
