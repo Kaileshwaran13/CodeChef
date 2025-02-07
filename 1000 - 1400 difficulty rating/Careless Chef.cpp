9dd45ca77faf1d7996ce6f0bc08908928bd68e2d
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2025-02-07 23:16:02 +0530

    Create Careless Chef.cpp

---
+++ 1000 - 1400 difficulty rating/Careless Chef.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int a[2 * n];
+        int sum = 0;
+        for (int i = 0; i < (2 * n); i++) {
+            cin >> a[i];
+            sum += a[i];
+        }
+        if (sum % 2) {
+            cout << "NO" << endl;
+        }
+        else cout << "YES" << endl;
+    }
+    return 0;
+}
