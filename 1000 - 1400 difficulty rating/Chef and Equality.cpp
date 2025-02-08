80f1a4e72660af0b90021564f0750af03c4a2b66
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2025-02-08 20:51:00 +0530

    Create Chef and Equality.cpp

---
+++ 1000 - 1400 difficulty rating/Chef and Equality.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int test;
+    cin >> test;
+    while (test--)
+    {
+        int n;
+        cin >> n;
+        map < int, int > mp;
+        int max_count = INT_MIN;
+        for (int i = 0; i < n; i++)
+        {
+            int x;
+            cin >> x;
+            mp[x]++;
+            max_count = max(max_count, mp[x]);
+        }
+        cout << n - max_count << endl;
+    }
+
+    return 0;
+}
