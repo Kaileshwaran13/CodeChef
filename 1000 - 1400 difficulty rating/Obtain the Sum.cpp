762c189a7d55e0f5eaee37d5b97b785d2686d470
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2025-02-08 19:04:16 +0530

    Create Obtain the Sum.cpp

---
+++ 1000 - 1400 difficulty rating/Obtain the Sum.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long int n, s;
+        cin >> n >> s;
+        long long int k = (n * (n + 1)) / 2;
+        int con = 0;
+        long long int p = k - s;
+
+        if (p >= 1 && p <= n) {
+            cout << p << endl;
+        }
+        else cout << -1 << endl;
+    }
+    return 0;
+}
