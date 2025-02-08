837ed6eb4e6bc05fb44c404267ef26d94db66fd8
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2025-02-08 19:01:52 +0530

    Create Another Game.cpp

---
+++ 1000 - 1400 difficulty rating/Another Game.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector < int > P(n);
+        for (int i = 0; i < n; i++) {
+            cin >> P[i];
+        }
+        int m = -1;
+        for (int i = 0; i < n; i++)
+        {
+            if ((i + 1) != (P[i]))
+                m = i + 1;
+        }
+        cout << m + 1 << endl;
+
+    }
+    return 0;
+}
