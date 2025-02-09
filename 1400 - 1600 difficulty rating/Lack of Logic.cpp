eca87c5ff84da911b1434f4358d6196294f09a4d
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2025-02-09 22:19:28 +0530

    Update Lack of Logic.cpp

--- 1400 - 1600 difficulty rating/Lack of Logic.cpp
+++ 1400 - 1600 difficulty rating/Lack of Logic.cpp
@@ -2,44 +2,33 @@
 using namespace std;
 
 int main() {
-    int T;
-    cin >> T;
-    while (T != 0) {
-        string s;
+    int t;
+    cin >> t;
+    string s;
+    getline(cin, s);
+    while (t--) {
         getline(cin, s);
+        map < char, int > m;
+        for (int i = 0; i < 26; i++) {
+            m[i + 'a'] = 1;
+        }
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] >= 'A' && s[i] <= 'Z') {
+                s[i] = s[i] - 'A' + 'a';
+            }
+            if (s[i] >= 'a' && s[i] <= 'z') {
+                m[s[i]]++;
+            }
 
-        if (s == "Father, what is the lack of logic?") {
-            cout << "Bunny";
         }
-        else if (s == "Where?") {
-            cout << "Day";
-        }
-        else {
-            unordered_map < char, bool > m;
-            for (int i = 0; i < s.length(); i++) {
-                if (s[i] >= 65 && s[i] <= 90) {
-                    m[s[i]] = true;
-                }
-                else if (s[i] >= 97 && s[i] <= 122) {
-                    m[s[i] - 32] = true;
-
-                }
-            }
-            bool flag = false;
-            for (int i = 65; i <= 90; i++) {
-                char elem = i;
-                if (m.count(elem) == 0) {
-                    cout << elem;
-                    flag = true;
-                    break;
-                }
-            }
-            if (flag == false) {
-                cout << "~";
+        char ans = '~';
+        for (auto it: m) {
+            if (it.second == 1) {
+                ans = it.first;
+                break;
             }
         }
-        cout << endl;
-        T--;
+        cout << ans << endl;
     }
-    return 0;
+
 }
