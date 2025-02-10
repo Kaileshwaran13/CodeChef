60b7e4651bb4cbc2ccbe3a94b05a47ea48953102
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2025-02-10 18:48:07 +0530

    Create Byte to Bit.cpp

---
+++ 1000 - 1400 difficulty rating/Byte to Bit.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        n--;
+        long long c = n / 26;
+        int rem = n % 26;
+        c = pow(2, c);
+
+        if (rem < 2)
+            cout << c << " 0 0" << endl;
+        else if (rem < 10)
+            cout << "0 " << c << " 0" << endl;
+        else
+            cout << "0 0 " << c << endl;
+    }
+    return 0;
+}
