4971757eed4c9890c3262078ee19aaa1269714e6
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2025-02-11 12:18:11 +0530

    Create Bit Play.cpp

---
+++ 1400 - 1600 difficulty rating/Bit Play.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define MAX INT_MAX
+
+int main() {
+    ll t;
+    cin >> t;
+    while (t--) {
+        ll n;
+        cin >> n;
+        string s;
+        cin >> s;
+        ll ans = 1;
+        for (ll i = 2; i < n; i += 2) {
+            ll ct = 0;
+            ll x = s[i - 1] - '0';
+            ll y = s[i - 2] - '0';
+            ll z = s[i] - '0';
+            if ((ll)(x & y) == z) ct++;
+            if ((ll)(x ^ y) == z) ct++;
+            if ((ll)(x | y) == z) ct++;
+            ans *= ct;
+            ans %= MAX;
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
