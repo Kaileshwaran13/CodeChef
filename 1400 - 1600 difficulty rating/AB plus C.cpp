eb2251ccc5331ff636e0edbd2ed2b0e0c2553065
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2025-02-12 22:34:54 +0530

    Create AB plus C.cpp

---
+++ 1400 - 1600 difficulty rating/AB plus C.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long x, a, b, c;
+        cin >> x;
+        if (x < 2) {
+            cout << -1 << endl;
+        } else if (x <= 1000000) {
+            cout << 1 << " " << 1 << " " << x - 1 << endl;
+        } else {
+            long long a = 1000000;
+            long long c = x % a;
+            long long b;
+            if (c == 0) {
+                b = (x / a) - 1;
+                c = a;
+            } else {
+                b = x / a;
+            }
+            cout << a << " " << b << " " << c << endl;
+        }
+    }
+    return 0;
+}
