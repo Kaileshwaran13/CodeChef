b745ef54f10d4c32ec489eeb6ac7e87b0cfca8ca
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2025-02-12 22:38:04 +0530

    Update Divisible by A_i.cpp

--- 1400 - 1600 difficulty rating/Divisible by A_i.cpp
+++ 1400 - 1600 difficulty rating/Divisible by A_i.cpp
@@ -10,12 +10,11 @@ int main()
         int n, hcf = 0;
         cin >> n;
         vector < int > v(n);
-        for(int i = a; i < b; i++){
+        for(int i = 0; i < n; i++){
             cin >> v[i];
             hcf = __gcd(hcf, v[i]);
         }
-        rep(i, 0, n)
-        {
+        for(int i = 0; i < n; i++){
             cout << v[i] / hcf << " ";
         }
         cout << endl;
