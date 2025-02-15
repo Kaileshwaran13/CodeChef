3707f0156f2b4e190ebfd3a2c37f0c27ebf6d9ca
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2025-02-15 23:02:40 +0530

    Create Coprime Range.cpp

---
+++ 1600 - 1800 difficulty rating/Coprime Range.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T = 1;
+    cin >> T;
+    while (T--) {
+        int l, r;
+        cin >> l >> r;
+        cout << (int) 1e9 + 7 << '\n';
+    }
+    return 0;
+}
