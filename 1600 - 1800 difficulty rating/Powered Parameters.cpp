4c4f8b7cac8c95dfefbab0ed97ec977e1b664d35
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2025-02-17 23:22:28 +0530

    Create Powered Parameters.cpp

---
+++ 1600 - 1800 difficulty rating/Powered Parameters.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long t;
+    cin >> t;
+    while (t--) {
+        long long n;
+        cin >> n;
+        vector < long long > v(n);
+        for (auto & i: v) cin >> i;
+        long long ans = 0;
+        for (int i = 0; i < n; i++) {
+            long long left = v[i];
+            if (v[i] == 1) 
+                ans += n;
+            else {
+                long long power = 1;
+                for (int j = 0; j < n; j++) {
+                    if (left * power > 1e9 + 10) break;
+                    else {
+                        power = left * power;
+                        if (power <= v[j]) ans++;
+                    }
+                }
+            }
+        }
+        cout << ans << '\n';
+
+    }
+
+}
