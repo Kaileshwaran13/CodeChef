3bdb2b3a51e459fe04129d8950e9b0672a475e1f
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2025-02-23 21:43:19 +0530

    Update Count Good Prefixes.cpp

--- 1600 - 1800 difficulty rating/Count Good Prefixes.cpp
+++ 1600 - 1800 difficulty rating/Count Good Prefixes.cpp
@@ -1,33 +1,55 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    int t;
-    int sum[1005];
-    char s[1005];
-    cin >> t;
-    while (t--) {
-        int n, l;
-        scanf("%s", s + 1);
-        cin >> n;
-        l = strlen(s + 1);
-        for (int i = 1; i <= l; i++) {
-            sum[i] = sum[i - 1] + (s[i] == 'a' ? 1 : -1);
-        }
-        long long ans = 0;
-        for (int i = 1; i <= l; i++) {
-            if (sum[l] == 0) {
-                if (sum[i] > 0) ans += n;
-            } else if (sum[l] > 0) {
-                int k = floor((-sum[i]) * 1.0 / sum[l]);
-                ans += max(0, n - 1 - max(-1, k));
-            } else {
-                int k = ceil((-sum[i]) * 1.0 / sum[l]);
-                ans += max(0, min(k, n));
+int main() {
+    int f;
+    cin >> f;
+    while (f--)
+    {
+        long long int n, i, j, count1 = 0, count2 = 0, count = 0, sum = 0, flag = 0, tempcount = 0;
+        char s[1000];
+        scanf("%s %lld", s, &n);
+        for (i = 0; i < n; i++)
+        {
+            tempcount = 0;
+            for (j = 0; j < strlen(s); j++)
+            {
+                if (s[j] == 'a')
+                    count1++;
+                else if (s[j] == 'b')
+                    count2++;
+                if (count1 > count2)
+                {
+                    count++;
+                    tempcount++;
+                }
+            }
+            if (count1 == count2)
+            {
+                flag = 1;
+                break;
+            }
+            if (tempcount == strlen(s))
+            {
+                flag = 2;
+                break;
+            }
+            if (tempcount == 0)
+            {
+                break;
             }
         }
-        cout << ans << '\n';
+        if (flag == 1)
+        {
+            sum = count * n;
+        }
+        else if (flag == 2)
+        {
+            sum = count + (n - 1 - i) * strlen(s);
+        }
+        else
+            sum = count;
+        printf("%lld\n", sum);
     }
     return 0;
 }
