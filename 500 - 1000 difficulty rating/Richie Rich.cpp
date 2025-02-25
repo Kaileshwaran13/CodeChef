7d09a7e5dac19dbe102f32f247eee5bf9b780e2e
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2025-02-25 23:09:58 +0530

    Create Richie Rich.cpp

---
+++ 500 - 1000 difficulty rating/Richie Rich.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int tt, a, b, c;
+    cin >> tt ;
+    while(tt--){
+        cin >> a >> b >> c;
+        cout << (b - a) / c << '\n';
+    }
+
+}
